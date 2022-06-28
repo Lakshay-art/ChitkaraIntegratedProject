97f6c86f236a4b81eb453b90a1b6f8a207acc586
Tarun Bhan Singh <tarunbhansingh@Taruns-MacBook-Pro.local>
2022-06-28 11:39:10 +0530

    added

--- pages/upload.js
+++ pages/upload.js
@@ -9,11 +9,8 @@ import Banner from '../components/Banner/Banner';
 import { Button, Flex, FlexBox, Wrap, WrapBox } from '../components/Banner/Banner.styles';
 import Filter from '../components/Filter';
 import DropMessage from '../components/DropMessage/DropMessage';
-<<<<<<< HEAD
 import About from './about';
-=======
 import Suggestion from '../components/Suggestion'
->>>>>>> main
 
 // import ByBase64 from '../components/ByBase64'
 
