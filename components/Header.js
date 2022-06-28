7586b8e9aa5b5add0beb5f6e17d426238d36d07e
Tarun Bhan Singh <tarunbhansingh@Taruns-MacBook-Pro.local>
2022-06-28 11:39:25 +0530

    added name

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import headerStyles from '../styles/Header.module.css'
-import About from '../pages/about'
 const Header = () => {
   return (
     <header className={headerStyles.mainheader}>
@@ -10,7 +9,7 @@ const Header = () => {
           <a className={headerStyles.a}>
             Home
           </a></Link>
-        <Link className={headerStyles.Link} href='/about' target="as_blank">
+        <Link className={headerStyles.Link} href='/aboutus' target="as_blank">
           <a className={headerStyles.a} >
             About
           </a>
